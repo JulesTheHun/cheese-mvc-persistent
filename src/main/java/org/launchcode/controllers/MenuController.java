21adec26dff5eff478ee6336229a116fe02b61da
Julian Hung <hungsjulian@gmail.com>
2018-12-25 09:22:05 -0600

    got up to views

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,70 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Menu;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model){
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddMenuForm(Model model){
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+        return "menu/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String processAddMenuForm(@ModelAttribute @Valid Menu newMenu, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+        }
+
+        menuDao.save(newMenu);
+
+        return "redirect:view/" + newMenu.getId();
+    }
+
+    @RequestMapping(value="view/{menuId}", method = RequestMethod.GET)
+    public String viewMenu(Model model, @PathVariable int menuId){
+        Menu menu = menuDao.findOne(menuId);
+        model.addAttribute("menu", menu);
+        model.addAttribute("title", menu.getName());
+
+        return "menu/view";
+    }
+
+    @RequestMapping(value="add-item/{menuId}", method = RequestMethod.GET)
+    public String addItem(Model model, @PathVariable int menuId){
+        Menu menu = menuDao.findOne(menuId);
+        model.addAttribute("title", "Add item to " + menu.getName());
+        return "menu/add-item";
+    }
+
+}
--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,13 +1,17 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Menu {
 
     @Id
     @GeneratedValue
@@ -17,14 +21,17 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
+    @ManyToMany
     private List<Cheese> cheeses = new ArrayList<>();
 
-    public Category(){}
+    public Menu(){}
 
-    public Category(String name) {
-        this.setName(name);
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -35,9 +42,11 @@ public class Category {
         this.name = name;
     }
 
-    public int getId(){
-        return id;
+    public List<Cheese> getCheeses() {
+        return cheeses;
     }
 
-    public List<Cheese> getCheeses() { return cheeses; }
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
 }
